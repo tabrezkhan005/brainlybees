4ffa30805e6e78c4c749c5b4966273906069a5ae
Sheema Syed <shma.syd975@gmail.com>
2025-09-27 16:41:47 +0530

    Modified home page

--- src/components/TrustedByParentsSection.tsx
+++ src/components/TrustedByParentsSection.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function TrustedByParentsSection() {
   return (
     <section className="bg-gradient-to-b from-orange-100 to-orange-50 py-20 relative overflow-hidden">
@@ -26,38 +28,14 @@ export default function TrustedByParentsSection() {
         <div className="relative mb-16">
           <div className="flex justify-center">
             <div className="relative w-full max-w-4xl">
-              {/* Simplified World Map */}
-              <div className="relative w-full h-96">
-                <svg viewBox="0 0 1000 500" className="w-full h-full">
-                  {/* Continents as simple shapes */}
-                  <g fill="#4F46E5" opacity="0.8">
-                    {/* North America */}
-                    <path d="M50,100 L200,80 L250,120 L200,200 L100,180 Z"/>
-                    {/* South America */}
-                    <path d="M180,250 L220,240 L240,350 L200,380 L160,340 Z"/>
-                    {/* Europe */}
-                    <path d="M450,80 L520,70 L550,120 L500,140 L430,120 Z"/>
-                    {/* Africa */}
-                    <path d="M480,150 L550,140 L580,280 L520,320 L460,280 Z"/>
-                    {/* Asia */}
-                    <path d="M550,60 L750,50 L800,120 L780,200 L520,180 Z"/>
-                    {/* Australia */}
-                    <path d="M720,300 L800,290 L820,330 L780,350 L700,340 Z"/>
-                  </g>
-                  
-                  {/* Location Pins */}
-                  <g fill="#EF4444">
-                    <circle cx="150" cy="120" r="8"/>
-                    <circle cx="200" cy="150" r="8"/>
-                    <circle cx="480" cy="100" r="8"/>
-                    <circle cx="520" cy="180" r="8"/>
-                    <circle cx="650" cy="90" r="8"/>
-                    <circle cx="700" cy="140" r="8"/>
-                    <circle cx="750" cy="320" r="8"/>
-                    <circle cx="780" cy="280" r="8"/>
-                  </g>
-                </svg>
-              </div>
+              <Image
+                src="/world-map.svg"
+                alt="World map showing countries where parents trust our service"
+                width={800}
+                height={400}
+                className="w-full h-auto"
+                priority
+              />
             </div>
           </div>
         </div>
